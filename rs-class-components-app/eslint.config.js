ac0e2e7323c1acd771e7e11ca6912286c6b4d107
selestrel07 <selestrel07@gmail.com>
2025-07-11 11:40:04 +0200

    chore: install and configure husky with lint-staged

--- rs-class-components-app/eslint.config.js
+++ rs-class-components-app/eslint.config.js
@@ -1,46 +1,49 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-import eslintPluginPrettier from "eslint-plugin-prettier";
-import react from "@vitejs/plugin-react";
-import reactCompiler from "eslint-plugin-react-compiler";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginReact from 'eslint-plugin-react';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default tseslint.config([
+  js.configs.recommended,
+  tseslint.configs.recommended,
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.strict,
-      eslintPluginPrettier,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "react-compiler": reactCompiler,
+      react: eslintPluginReact,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'react-compiler': reactCompiler,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
+      ...eslintPluginReact.configs.recommended.rules,
+      ...eslintPluginReact.configs['jsx-runtime'].rules,
+      'react-refresh/only-export-components': [
+        'warn',
         { allowConstantExport: true },
       ],
-      "react-compiler/react-compiler": "error",
-      ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
+      'react-compiler/react-compiler': 'error',
+      'prettier/prettier': 'warn',
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
-])
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+  },
+]);
