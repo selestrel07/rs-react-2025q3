21c0d0aaa350a6d74b8261224773c4729f0c57e4
selestrel07 <selestrel07@gmail.com>
2025-07-17 16:41:16 +0200

    chore: install and configure vitest with dependencies

--- rs-class-components-app/vite.config.ts
+++ rs-class-components-app/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest/config" />
+import { defineConfig, type UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/test-utils/test-setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: 'text',
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['src/**/*.test.{ts,tsx}', 'src/**/*.d.ts'],
+    },
+    coverageThreshold: {
+      global: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
+} as UserConfig);
