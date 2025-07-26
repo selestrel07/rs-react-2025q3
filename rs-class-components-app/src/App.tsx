cc62ef0bfdf3a43e1290e5fad676cb263de5b960
selestrel07 <selestrel07@gmail.com>
2025-07-26 19:52:37 +0200

    feat: add browser router with path to main page

--- rs-class-components-app/src/App.tsx
+++ rs-class-components-app/src/App.tsx
@@ -1,19 +1,25 @@
 import './App.css';
 import { MainPage } from './pages/Main.tsx';
 import { ErrorBoundary } from './features/error-boundary/ErrorBoundary.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { Path } from './enums/Path.ts';
 
 function App() {
   return (
-    <ErrorBoundary
-      fallback={
-        <p className="error-container">
-          Something went wrong. Please check the console to see the error
-          message.
-        </p>
-      }
-    >
-      <MainPage />
-    </ErrorBoundary>
+    <BrowserRouter>
+      <ErrorBoundary
+        fallback={
+          <p className="error-container">
+            Something went wrong. Please check the console to see the error
+            message.
+          </p>
+        }
+      >
+        <Routes>
+          <Route path={Path.MAIN} element={<MainPage />} />
+        </Routes>
+      </ErrorBoundary>
+    </BrowserRouter>
   );
 }
 
