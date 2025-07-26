454e24edb3878e1f9d21cdaa62e0529d4fb4c9aa
selestrel07 <selestrel07@gmail.com>
2025-07-26 11:53:09 +0200

    refactor: move ArtistCard properties type to the separate file

--- rs-class-components-app/src/features/artists/ArtistCard.tsx
+++ rs-class-components-app/src/features/artists/ArtistCard.tsx
@@ -1,13 +1,9 @@
 import { type FC, type ReactNode } from 'react';
-import type { ArtistInfo } from '../../types/artist-data.ts';
 import './ArtistCard.css';
+import type { ArtistCardProperties } from '../../types/component-properties.ts';
 
-type ArtistCardProps = {
-  artist: ArtistInfo;
-};
-
-export const ArtistCard: FC<ArtistCardProps> = (
-  props: ArtistCardProps
+export const ArtistCard: FC<ArtistCardProperties> = (
+  props: ArtistCardProperties
 ): ReactNode => {
   return (
     <div className="card">
---
+++ rs-class-components-app/src/types/component-properties.ts
@@ -0,0 +1,5 @@
+import type { ArtistInfo } from './artist-data.ts';
+
+export type ArtistCardProperties = {
+  artist: ArtistInfo;
+};
