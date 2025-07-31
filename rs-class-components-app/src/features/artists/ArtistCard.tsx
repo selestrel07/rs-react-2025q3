4818f535d62a88765aa644c0b2da9eb407ec4fbb
selestrel07 <selestrel07@gmail.com>
2025-07-31 12:46:45 +0200

    feat: implement item selection functionality

--- rs-class-components-app/src/features/artists/ArtistCard.tsx
+++ rs-class-components-app/src/features/artists/ArtistCard.tsx
@@ -1,18 +1,42 @@
-import { type FC, type ReactNode } from 'react';
+import {
+  type FC,
+  type ReactNode,
+  useState,
+  type MouseEvent,
+  useEffect,
+} from 'react';
 import './ArtistCard.css';
 import type { ArtistCardProperties } from '../../types/component-properties.ts';
+import { getArtists, store } from '../../store.ts';
+import { addArtistAction, removeArtistAction } from './artistSlice.ts';
 
 export const ArtistCard: FC<ArtistCardProperties> = (
   props: ArtistCardProperties
 ): ReactNode => {
+  const appStore = store;
+  const [isSelected, setSelected] = useState(
+    getArtists()
+      .map((artist) => artist.id)
+      .includes(props.artist.id)
+  );
+
+  useEffect(() => {
+    if (isSelected) {
+      appStore.dispatch(addArtistAction(props.artist));
+    } else {
+      appStore.dispatch(removeArtistAction(props.artist));
+    }
+  }, [isSelected, props.artist, appStore]);
+
+  const handleClick = (e: MouseEvent<HTMLElement>): void => {
+    e.stopPropagation();
+    props.navigate(props.artist.id);
+    setSelected(!isSelected);
+  };
+
   return (
-    <div
-      className="card"
-      onClick={(e) => {
-        e.stopPropagation();
-        props.navigate(props.artist.id);
-      }}
-    >
+    <div className="card" onClick={handleClick}>
+      <input type="checkbox" checked={isSelected} />
       <p>
         <b>Title: </b>
         {props.artist.title}
