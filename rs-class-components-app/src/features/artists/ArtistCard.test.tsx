77b765ebaf5896e241476812b152129a93f3dea5
selestrel07 <selestrel07@gmail.com>
2025-08-01 16:26:18 +0200

    fix: update tests with provider

--- rs-class-components-app/src/features/artists/ArtistCard.test.tsx
+++ rs-class-components-app/src/features/artists/ArtistCard.test.tsx
@@ -6,16 +6,24 @@ import {
   ArtistInfoFull,
 } from '../../test-utils/test-data.ts';
 import { userEvent } from '@testing-library/user-event';
+import { Provider } from 'react-redux';
+import { store } from '../../store.ts';
+import type { ReactNode } from 'react';
+
 const getElementByText = (elementText: string) =>
   screen.getByText(
     (_, element) => element !== null && element.textContent === elementText
   );
 
 const navigateMock = vi.fn();
+const renderElement = (child: ReactNode) =>
+  render(<Provider store={store}>{child}</Provider>);
 
 describe('ArtistCard render test', () => {
   it('Should render artist data', async () => {
-    render(<ArtistCard artist={ArtistInfoFull} navigate={navigateMock} />);
+    renderElement(
+      <ArtistCard artist={ArtistInfoFull} navigate={navigateMock} />
+    );
 
     expect(
       getElementByText(`Title: ${ArtistInfoFull.title}`)
@@ -27,7 +35,7 @@ describe('ArtistCard render test', () => {
   });
 
   it('Should render question mark if date is null', async () => {
-    render(
+    renderElement(
       <ArtistCard artist={ArtistInfoEmptyDates} navigate={navigateMock} />
     );
 
@@ -39,7 +47,7 @@ describe('ArtistCard render test', () => {
   });
 
   it('Should trigger navigate function on click', async () => {
-    render(
+    renderElement(
       <ArtistCard artist={ArtistInfoEmptyDates} navigate={navigateMock} />
     );
 
