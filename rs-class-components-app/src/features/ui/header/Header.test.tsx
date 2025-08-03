259efad22b6fa1aa03627c777688684c1ceb370b
selestrel07 <selestrel07@gmail.com>
2025-08-03 13:36:35 +0200

    chore: update Header component tests

--- rs-class-components-app/src/features/ui/header/Header.test.tsx
+++ rs-class-components-app/src/features/ui/header/Header.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest';
 import { Header } from './Header.tsx';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { BrowserRouter, MemoryRouter } from 'react-router';
 import { ABOUT } from '../../../data/path-constants.ts';
 import { ThemeContextProvider } from '../../../context/ThemeContext.tsx';
@@ -19,7 +19,21 @@ describe('Header component tests', () => {
     expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
   });
 
-  it('Test active and inactive link colors', () => {
+  it('Should contain theme change button', () => {
+    render(
+      <BrowserRouter>
+        <ThemeContextProvider>
+          <Header />
+        </ThemeContextProvider>
+      </BrowserRouter>
+    );
+
+    expect(
+      screen.getByRole('button', { name: 'Change theme to dark' })
+    ).toBeInTheDocument();
+  });
+
+  it('Should change the app theme on the change theme button click', () => {
     render(
       <MemoryRouter initialEntries={[ABOUT]}>
         <ThemeContextProvider>
@@ -27,6 +41,21 @@ describe('Header component tests', () => {
         </ThemeContextProvider>
       </MemoryRouter>
     );
+
+    const changeThemeButton = screen.getByRole('button', {
+      name: 'Change theme to dark',
+    });
+
+    act(() => changeThemeButton.click());
+
+    expect(screen.getByRole('link', { name: 'Main' })).toHaveStyle(
+      'color: #4be42e'
+    );
+    expect(screen.getByRole('link', { name: 'About' })).toHaveStyle(
+      'color: #f8e8e9'
+    );
+
+    act(() => changeThemeButton.click());
 
     expect(screen.getByRole('link', { name: 'Main' })).toHaveStyle(
       'color: #3b6c28'
