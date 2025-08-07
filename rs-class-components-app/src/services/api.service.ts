1febbe5fb8d7fed9893858a1f144afaab383bf72
selestrel07 <selestrel07@gmail.com>
2025-08-07 12:34:19 +0200

    feat: configure RTK Query

--- rs-class-components-app/src/services/api.service.ts
+++ rs-class-components-app/src/services/api.service.ts
@@ -1,9 +1,35 @@
-import type { SearchResult } from '../types/search-item.ts';
-import type { ArtistData } from '../types/artist-data.ts';
+import type { SearchItem, SearchResult } from '../types/search-item.ts';
+import type { ArtistData, ArtistInfo } from '../types/artist-data.ts';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const BASE_URL = 'https://api.artic.edu/api/v1/artists/search';
 const BASE_ARTIST_URL = 'https://api.artic.edu/api/v1/agents/';
 const ENTITY_LIMIT = 8;
+
+type SearchParameters = {
+  queryString: string;
+  page: number;
+};
+
+export const artistsApi = createApi({
+  reducerPath: 'artistsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.artic.edu/api/v1/',
+  }),
+  endpoints: (build) => ({
+    searchArtist: build.query<SearchItem[], SearchParameters>({
+      query: ({ queryString, page }) =>
+        `artists/search/?limit=${ENTITY_LIMIT}&page=${page}&q=${queryString}`,
+      transformResponse: (rawResult: SearchResult) => rawResult.data,
+    }),
+    getArtist: build.query<ArtistInfo, string>({
+      query: (id: string) => `agents/${id}`,
+      transformResponse: (rawResult: ArtistData) => rawResult.data,
+    }),
+  }),
+});
+
+export const { useSearchArtistQuery, useGetArtistQuery } = artistsApi;
 
 const composeUrl = (pageNumber: number, queryString?: string): URL => {
   const url = new URL(BASE_URL);
--- rs-class-components-app/src/store.ts
+++ rs-class-components-app/src/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { artistsReducer } from './features/artists/artistSlice.ts';
+import { artistsApi } from './services/api.service.ts';
 
 export const store = configureStore({
   reducer: {
     artists: artistsReducer,
+    [artistsApi.reducerPath]: artistsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(artistsApi.middleware),
 });
 
 export type AppState = ReturnType<typeof store.getState>;
