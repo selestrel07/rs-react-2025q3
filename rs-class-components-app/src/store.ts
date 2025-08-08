c537a8a713efd5242871784d546a0d87471c021a
selestrel07 <selestrel07@gmail.com>
2025-08-08 10:57:52 +0200

    feat: add search query redux state

--- rs-class-components-app/src/store.ts
+++ rs-class-components-app/src/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { artistsReducer } from './features/artists/artistSlice.ts';
 import { artistsApi } from './services/api.service.ts';
 import { paginationReducer } from './features/paginaton/paginationSlice.ts';
+import { searchQueryReducer } from './features/search/searchQuerySlice.ts';
 
 export const store = configureStore({
   reducer: {
     artists: artistsReducer,
     pagination: paginationReducer,
+    searchQuery: searchQueryReducer,
     [artistsApi.reducerPath]: artistsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
