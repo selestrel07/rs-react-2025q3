2abcc5757e083076660f7dcb5cf873c6b4b2e2b6
selestrel07 <selestrel07@gmail.com>
2025-08-23 12:03:16 +0200

    chore: update eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import react from "eslint-plugin-react";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -11,13 +14,33 @@ export default tseslint.config([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      ...tseslint.configs.strict,
+      eslintPluginPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "react-compiler": reactCompiler,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react-compiler/react-compiler": "error",
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
 ])
