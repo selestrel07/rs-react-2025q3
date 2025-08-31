036831a769577d84e2882d32140fce420b0249be
selestrel07 <selestrel07@gmail.com>
2025-08-31 10:19:37 +0200

    feat: update loader to display in the middle of page

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -2,14 +2,16 @@ import { type FC } from 'react';
 
 const Loader: FC = () => {
   return (
-    <div className="w-3.5 h-3.5 animate-spin m-auto">
-      <div className="w-full h-1/2 flex justify-between">
-        <div className="w-1.5 h-1.5 bg-blue-100"></div>
-        <div className="w-1.5 h-1.5 bg-blue-200"></div>
-      </div>
-      <div className="w-full h-1/2 flex justify-between">
-        <div className="w-1.5 h-1.5 bg-blue-400"></div>
-        <div className="w-1.5 h-1.5 bg-blue-300"></div>
+    <div className="flex items-center h-screen">
+      <div className="w-3.5 h-3.5 animate-spin m-auto">
+        <div className="w-full h-1/2 flex justify-between">
+          <div className="w-1.5 h-1.5 bg-blue-100"></div>
+          <div className="w-1.5 h-1.5 bg-blue-200"></div>
+        </div>
+        <div className="w-full h-1/2 flex justify-between">
+          <div className="w-1.5 h-1.5 bg-blue-400"></div>
+          <div className="w-1.5 h-1.5 bg-blue-300"></div>
+        </div>
       </div>
     </div>
   );
